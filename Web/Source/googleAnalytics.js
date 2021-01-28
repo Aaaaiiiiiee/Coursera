f870f43251dc8c702c39b13ec396f766c7c36ab8
Aaaaiiiiiee <wjjgjk@naver.com>
2021-01-28 10:46:47 +0000

    Add GoogleAnalytics

--- Web/Source/googleAnalytics.js
+++ Web/Source/googleAnalytics.js
@@ -1,1 +1,10 @@
-document.querySelector('head').append("<!-- Global site tag (gtag.js) - Google Analytics --<script async src='https://www.googletagmanager.com/gtag/js?id=G-KB6MQ9M805'></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-KB6MQ9M805');</script>");
+var code1 = document.createElement('script');
+var code2 = document.createElement('script');
+
+code1.setAttribute('async', '');
+code1.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-KB6MQ9M805");
+
+code2.innerHTML = "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-KB6MQ9M805');";
+
+document.querySelector('head').appendChild(code1);
+document.querySelector('head').appendChild(code2);
