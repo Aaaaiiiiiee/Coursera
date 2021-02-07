bc76a2dd61e55c9116ea073e95cf760e2a064a8a
Aaaaiiiiiee <wjjgjk@naver.com>
2021-02-07 10:47:24 +0000

    Add Router

--- Web/Source/web3-express/main.js
+++ Web/Source/web3-express/main.js
@@ -1,16 +1,15 @@
 /* default module */
 var fs = require('fs');
-var path = require('path');
-var qs = require('querystring');
 
 /* npm install module*/
 const express = require('express');
 const app = express();
-var sanitizeHtml = require('sanitize-html');
 var bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 /* personal module*/
-var template = require('./lib/template.js');
+var topicRouter = require('./routes/topic');
+var indexRouter = require('./routes/index');
 
 // How to Use MiddleWare.
 // * Create `.body` in `request` parameter. Run both POST and GET.
@@ -28,127 +27,8 @@ app.use(express.static('public'));
 app.use(helmet());
 
 /* Add GET, POST link */
-app.get('/', (request, response) => {
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-        `
-        <h2>${title}</h2>${description}
-        <img src="/images/hello.jpg" style="width:300px; display: block; margin-top: 10px;">
-        `,
-        `<a href="/topic/create">create</a>`
-    );
-    response.send(html);
-});
-
-app.get('/topic/create', (request, response)=>{
-    var title = 'WEB - create';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list, `
-          <form action="/topic/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `, '');
-    response.send(html);
-});
-
-app.post('/topic/create_process', (request, response)=>{
-    var post = request.body;
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.redirect(`/topic/${title}`);
-    })
-});
-
-app.get('/topic/update/:pageId', (request, response)=>{
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        var title = request.params.pageId;
-        var list = template.list(request.list);
-        var html = template.HTML(title, list,
-            `
-            <form action="/topic/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/topic/update/${title}">update</a>`
-        );
-        response.send(html);
-    });
-});
-
-app.post('/topic/update_process', (request, response) => {
-    var post = request.body;
-
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-            response.redirect(`/topic/${title}`);
-        })
-    });
-});
-
-app.post('/topic/delete_process', (request, response) => {
-    var post = request.body;
-
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function (error) {
-        // response.writeHead(302, { Location: `/` });
-        // response.end();
-        response.redirect('/');
-    })
-});
-
-app.get('/topic/:pageId', (request, response, next) => {
-    /*
-        ** if URL is /page/HTML ...
-        request.params = {
-            pageId: 'HTML'
-        }
-    */
-
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        if(err){
-            next(err);
-        }else{
-            var title = request.params.pageId;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-                allowedTags: ['h1']
-            });
-            var list = template.list(request.list);
-            var html = template.HTML(sanitizedTitle, list,
-                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                ` <a href="/topic/create">create</a>
-                <a href="/topic/update/${sanitizedTitle}">update</a>
-                <form action="/topic/delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-            );
-            response.send(html);
-        }
-    });
-});
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
 
 /* Page not Found (404) */
 app.use((req, res, next)=>{
