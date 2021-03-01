2252785c2f90b1034d0c0dca575ec226ee350cdb
Aaaaiiiiiee <wjjgjk@naver.com>
2021-03-01 04:13:48 +0000

    2021-03-01 socket.io.example Practice (1)

---
+++ socket.io/socket.io.example/examples/private-messaging/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  configureWebpack: {
+    // other webpack options to merge in ...
+  },
+  // devServer Options don't belong into `configureWebpack`
+  devServer: {
+    public: '10.4.2.30',
+    hot: true,
+    disableHostCheck: true,
+  },
+};
